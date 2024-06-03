3e51528d5644e8812655108eee23845578b185cb
Sagar-Landge <sagarlandge593@gmail.com>
2024-06-03 11:13:12 +0530

    last commit

--- src/main/java/com/example/demo/controller/UsersController.java
+++ src/main/java/com/example/demo/controller/UsersController.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entities.Song;
 import com.example.demo.entities.Users;
